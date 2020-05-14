0a85d53530306df82f4517f70dda4793cda51ef1
It.Store srl <46936595+Itpuntostore@users.noreply.github.com>
2020-05-14 16:49:46 +0200

    Add files via upload

---
+++ languages/lang_it.php
@@ -0,0 +1,112 @@
+<?php
+/* 
+------------------
+Language: Italiano
+------------------
+*/
+
+$lang = array();
+
+//Menù
+$lang['menu_home'] = "Home";
+$lang['menu_biografia'] = "Biografia";
+$lang['menu_music'] = "Musica";
+$lang['menu_discografia'] = "Discografia";
+$lang['menu_newseventi'] = "Eventi";
+$lang['menu_contest'] = "Contest";
+$lang['menu_vocalcoach'] = "Vocal Coach";
+$lang['menu_photogallery'] = "Foto Gallery";
+$lang['menu_MBlab'] = "MBlab";
+$lang['menu_gadget'] = "Gadget";
+$lang['menu_contatti'] = "Contatti";
+$lang['menu_credits'] = "Credits";
+
+//Home
+$lang['home_title'] = "MATTEO BECUCCI<br/>OFFICIAL WEBSITE";
+$lang['home_subtitle'] = "le parole e le cose";
+
+//Contest
+$lang['contest_title'] = "Matteo Becucci";
+$lang['contest_subtitle'] = "Contest";
+
+//Biografia
+$lang['biografia_title'] = "Matteo Becucci"; 
+$lang['biografia_subtitle'] = "Biografia";
+/*$lang['biografia_description_p1'] = "Matteo Becucci nasce a Livorno il 4 Agosto 1970 ed � per colpa o merito di Nonna Lea, insegnante di pianoforte, che Matteo inizia a conoscere il meraviglioso mondo della musica.  
+Frequenta il liceo linguistico, appassionato dalla lingua inglese che lo avvicina ai suoi grandi miti della canzone internazionale: Sting, Prince, Elton John, Freddie Mercury e in quegli anni inizia a scrivere le sue prime canzoni.";
+$lang['biografia_description_p2'] = "A 17 anni prende la chitarra e, sulle scale del Liceo, canta sulle note di 'Hey Jude'. I suoi compagni di classe restano ammutoliti dalla bravura e dalla profondit� di questa improvvisazione, ed � solo l'inizio...perch� per Matteo tutto � naturale, spontaneo. Il canto � la sua natura. Ma ancora non si sente pronto per esibirsi davanti ad un pubblico e si limita a cantare tra se' e se', nascosto nel casco mentre guida il motorino, nel bagno della scuola, nella sua camera..";
+$lang['biografia_description_p3'] = "A 18 compra una chitarra classica e comincia a prendere lezioni da Roberto Sbolci. Scrive ancora canzoni. A 20 anni il debutto live esibendosi in locali di Livorno e provincia accompagnato da Riccardo Torri (Chitarra) e Claudio Fabiani (Flauto/Sax).";
+$lang['biografia_description_p4'] = "Nel 1993 ,dopo aver fatto esperienze con varie formazioni, diventa il cantante di un gruppo di Castagneto Carducci denominato Rien a' Faire con il quale accompagner� Sergio Caputo nei Tour degli anni 1994 e 1995. Nel 1995 conosce Franco Ceccanti (chitarra) e forma un duo denominato Check Back, ancora oggi in attivit� con il nome di Check Forward (da pi� di dieci anni si esibiscono in club italiani ed esteri). Dal 1997 collabora con un gruppo lucchese, i Mr Pitiful con i quali si avvicina in punta di piedi al Soul di Menphis e soprattutto alla passione per il grande Stevie Wonder. Contemporaneamente entra a far parte dei Plisdebill, capitanati da Marco Baracchino (funk/disco '70), coi quali registra un CD. Nel 1998 partecipa con i Rien a' faire al Trofeo Roxy Bar di Red Ronnie. Nel 1998 e 1999 partecipa ad alcuni Festival di prestigio dedicati alla musica Soul fra i quali il Porretta Soul Festival. Nel 2000 registra la canzone colonna sonora "Come una cosa andro'" del Film "Non con un bang" , presentato al Festival del cinema di Venezia per la regia di Mariano Lamberti e colonna sonora di Roberto Sbolci. Nel 2003 con i Mr Pitiful partecipa, come autore e voce solista, ad un progetto di brani inediti dando vita ad un CD dal titolo "Liberi di Mente".Dal 2003 comincia, grazie alla passione per Kurt Elling, ad avvicinarsi al canto jazz. Nello stesso anno collabora con i City Garden di Vince Bramanti. Nel 2005 partecipa alla presentazione live del disco Nio Tio di Marco Baracchino come vocalist.";
+$lang['biografia_description_p5'] = "Dal 2008 � la voce maschile di un gruppo di 10 elementi voluto da Leo Damiani e assemblato da Andrea Lombardo denominato Bubble Sound Orchestra. Questa formazione si esibisce prevalentemente come colonna sonora live di accompagnamento ad eventi del mondo "vip" atti a pubblicizzare un prodotto Franciacorta di un noto produttore di vini italiano. 
+Nel gennaio 2009 Matteo partecipa alla trasmissione "X Factor". Senza troppe ambizioni e speranze riesce infatti a superare i numerosi casting fino ad entrare nella rosa dei 12 concorrenti in trasmissione. Sotto le grinfie del mitico Morgan, Marco Castoldi, si distingue per la professionalit� ed espressione canora che lo portano piano piano e sottovoce a raggiungere la vittoria del programma. 
+La fine del programma e la sua vittoria sono segnati dalla pubblicazione dell' EP 'Impossibile' (Rca/Sony Music) contenente, tra gli altri, il brano omonimo 'Impossibile' al quale ha partecipato come autore. 
+L'estate 2009 lo vede protagonista in tutta Italia di numerosi concerti come cantante solista e come punta di diamante per l'"X Factor tour".";
+$lang['biografia_description_p6'] = "A Novembre 2009 dopo mesi di intenso lavoro con la collaborazione di grandi artisti del calibro di Lucio Fabbri e Pippo Kaballa', Matteo � pronto per la nuova sfida: uscire sul mercato con il suo lavoro: "Cioccolato Amaro & Caffe'". Album raffinato ed elegante, musica di qualit�,  successi internazionali del calibro di "Smoke on the Water" dei Deep Purple e "Fragile" di Sting  rielaborati nell' arrangiamento e tradotti in lingua italiana.";
+$lang['biografia_description_p7'] = "Nell'Estate 2010 Matteo diventa GIUDA nel musical "Jesus Christ Superstar" per la regia di Massimo Romeo Piparo. Una tournee di oltre 80 date in tutta Italia che si conclude nel  febbraio 2011 . Insieme a Matteo un  cast ricco di nomi della musica leggera italiana come Max Gazze', Mario Venuti e Simona Bencini (dirotta su Cuba).";
+$lang['biografia_description_p8'] = "Il 3 Maggio 2011 anticipato dal singolo "La Cucina Giapponese" - esce l'album "Matteo Becucci"il suo terzo album. Il primo vero e proprio lavoro di brani inediti con la collaborazione di Pippo Kaballa', Mario Venuti, Luca Chiaravalli, Pio Stefanini, Francesco Sighieri, Massimo Greco, Pier Cortese, Valentin Gerlier.";
+$lang['biografia_description_p9'] = "L'autunno/inverno 2011/12 vede Matteo impegnato nell'allestimento di uno spettacolo natalizio insieme al quartetto di archi "Archimia" con i quali propone inizialmente un repertorio classico e non  con i brani della migliore tradizione natalizia per poi spaziare in un repertorio pop di brani propri e covers  italiane ed internazionali. Tra I vari concerti live in teatri e piazze anche il palcoscenico del "Blue Note" di Milano nel Marzo del 2012.";
+$lang['biografia_description_p10'] = "A maggio 2012 si torna a teatro questa volta nei panni di "Napoleone" nel cartoon musical "Mangiaresti il Gatto di Napoleone" scritto da Antonio Frecentese per la regia di Franco Travaglio con le musiche di Giovanni Giannini; una piccola produzione dove Matteo debutta in un ruolo che oltre al canto richiede una buona parte di recitazione.";
+$lang['biografia_description_p11'] = "A Ottobre 2012 Matteo entra nel cast dello spettacolo "Ti Ricordi il Varieta" di e con Vittorio Gucci e Cristina Chiabotto in giro nei teatri italiani per la stagione invernale 2012/ 2013. Continua allo stesso tempo ad esibirsi nei teatri/jazz club con lo spettacolo "MINIJAZZ" accompagnato da Nino Pellegrini e Antonio Masoni.";
+$lang['biografia_description_p12'] = "E cosi, il 19 aprile 2009 vinceva X-Factor 2,il 19 aprile 2013 usciva  "Fammi dormire" un nuovo singolo. In rotazione radiofonica e su tutte le piattaforme digitali, FAMMI DORMIRE e' un brano che parla di un amore perduto. Una canzone che � dichiarazione d'amore. Un brano pop che strizza l'occhio allo swing e al reggae.
+Questo singolo di Matteo Becucci torna a far sognare con la dolcezza e la forza della sua voce.";
+$lang['biografia_description_p13'] = "Per l'estate del 2014 Matteo pubblica il singolo "L'onda del destino" insieme al rapper Capitan America su etichetta Senza Base Records. - "I miei recenti ascolti di Jack Johnson e Ben Harper - afferma Matteo - la loro leggerezza, il mito del surf e tutte quelle sonorit� che il pop ha ereditato dal grande Bob Marley e affini mi hanno risuonato dentro per mesi fino ad ispirarmi questa song. Il giorno che l'ho scritta ero a casa dell'amico Andrea Chiarini (Capitan America). Pensando ad un rapper con cui duettare ho scoperto che il giovane amico oltre a rappare con una gran bella voce e' anche un chitarrista classico diplomato in conservatorio ed ha una passione per i Guns N' Roses. Dentro di me ho pensato dove potessi trovare un altro cos�? Cos� e' nato il tutto!"";
+$lang['biografia_foto_title'] = "Matteo Becucci Official";
+$lang['biografia_foto_button'] = "Foto Gallery";*/
+
+//Discografia
+$lang['discografia_viewintoitunes']="Visualizza in Itunes";
+
+//Photogallery
+$lang['photogallery_title'] = "Foto Gallery";
+$lang['photogallery_subtitle'] = "Matteo Becucci";
+$lang['photogallery_Album_all'] = "Tutte";
+$lang['photogallery_Album_xfactor'] = "X Factor";
+$lang['photogallery_Album_jesuschrist'] = "Jesus Chirst Superstar<br />Angelo Trani";
+$lang['photogallery_Album_monicasilva'] = "Foto d'autore<br />Monica Silva";
+$lang['photogallery_Album_enricoparrini'] = "Foto d'autore<br />Enrico Parrini";
+$lang['photogallery_Album_jadeeleonoire'] = "Foto d'autore<br />Jade Eleonoire";
+$lang['photogallery_Album_mangiaresti'] = "Mangiaresti Il Gatto di Napoleone";
+$lang['photogallery_Album_paoloscali'] = "Foto d'autore<br/>Paolo Scali";
+$lang['photogallery_Album_paoloscali2'] = "Foto d'autore<br/>Paolo Scali 2";
+$lang['photogallery_Album_FotoDiBea'] = "Foto di Bea";
+
+//Vocalcoach
+$lang['vocalcoach_title'] = "Matteo Becucci";
+$lang['vocalcoach_subtitle'] = "Vocal coach";
+$lang['vocalcoach_description_p1'] = "Insegnamento:";
+$lang['vocalcoach_description_p2'] = "sin da quando ho 20 anni � capitato spesso che persone, dopo avermi sentito cantare, mi abbiano  chiesto delle lezioni di canto. Io mi sono sempre rifiutato di farlo poich� in quanto autodidatta non avrei veramente saputo nemmeno da dove cominciare un percorso di insegnamento con chiunque. ";
+$lang['vocalcoach_description_p3'] = "Improvvisarsi insegnanti di un qualcosa che si conosce soltanto a livello empirico senza avere idea di come trasmetterlo mi avrebbe fatto correre rischio da un lato non insegnare un bel niente, dall�altro di non rispettare ci� per me � sacro, la musica ed in particolare il canto.";
+$lang['vocalcoach_description_p4'] = "Questo aspetto della mia vita � cambiato nel momento in cui anni fa ho conosciuto Alex Mastromarino. Alex � un insegnante , un performer di musical, un attore , un artista FANTASTICO con un curriculum impressionante. Collaborando con la scuola di cui lui � direttore, la WOS Academy di Livorno,ho cominciato ad avvicinarmi al mondo dell�insegnamento dapprima come artista che metteva la propria esperienza e cultura musicale al servizio degli studenti e gradualmente anche come insegnante vero e proprio. ";
+$lang['vocalcoach_description_p5'] = "La WOS Academy � una realt� in cui la materia canto viene trattata a 360� con  programmi giornalieri strutturati come corsi universitari . Un vero e proprio vanto per una citt� piccola come Livorno.";
+$lang['vocalcoach_description_p6'] = "Grazie ad Alex infatti ho cominciato a studiare un metodo vocale , il VMS.";
+$lang['vocalcoach_description_p7'] = "Il VMS (Vocal Music System) � un metodo vocale che , partendo dallo studio dell�anatomia ovvero la conformazione ed il funzionamento della laringe e di tutti gli altri elementi anatomici coinvolti nella fonazione , fornisce una serie di informazioni che spiegano scientificamente l�origine delle varie tipologie di suono che la voce umana pu� emettere e, attraverso esercizi mirati , come emetterli.";
+$lang['vocalcoach_description_p8'] = "Tutta questa �scienza� applicata al canto viene poi integrata ed applicata nello studio dei vari stili di canto moderno e completata dallo studio di solfeggio e pianoforte complementare.";
+$lang['vocalcoach_description_p9'] = "Sono rimasto folgorato da tanta completezza.";
+$lang['vocalcoach_description_p10'] = "L�artefice di questa meraviglia � un�insegnante di canto : Loretta Martinez.";
+$lang['vocalcoach_description_p11'] = "Da quando la conosco io dico che Loretta � la mia insegnante e lo penso davvero perch� studiando il suo metodo ho cominciato ad avere consapevolezza del mio cantare. Una consapevolezza che � anche scientifica.";
+$lang['vocalcoach_description_p12'] = "Cos� ho cominciato anche ad insegnare canto VMS.";
+$lang['vocalcoach_description_p13'] = "All�interno della WOS sono docente di Repertorio e  Stile e devo dire che provo una grande soddisfazione anche nell�insegnare.";
+$lang['vocalcoach_description_p14'] = "Mi capita inoltre di essere contattato per fare degli stage o di insegnare in altre scuole, una di queste � la Music Tribe di Barberino Val d�Elsa (FI).";
+
+
+//MBlab
+$lang['MBlab_title'] = "il genio e' al lavoro";
+$lang['taleequaleshow_title'] = "Tale & Quale Show in prima serata su Rai Uno";
+$lang['taleequaleshow_date'] = "dal 12/09/2014<br />��������ogni venerd�";
+$lang['taleequaleshow_description'] = "Carlo Conti torna da venerd� 12 settembre su Rai1, alle ore 21.10, con 'Tale e Quale Show', il supervariet� campione di ascolti della scorsa stagione televisiva, giunto quest'anno alla sua quarta edizione. Un cast tutto nuovo inizier� una sfida che proseguir� per undici puntate fino al 28 novembre.Pronti a mettersi in gioco imitando in tutto e per tutto i big della musica di oggi e di ieri, i dodici protagonisti, due in pi� dello scorso anno, sei uomini e sei donne. In gara quest'anno a contendersi la vittoria finale ci saranno: Michela Andreozzi, Luca Barbareschi, Matteo Becucci, Raffaella Fico, Rita Forte, Roberta Giarrusso, Alessandro Greco, Pino Insegno, Veronica Maya, Gianni Nazzaro, Serena Rossi, Valerio Scanu. <br /><br />Vi aspettiamo per seguire la diretta e commentare live @ ";
+$lang['taleequaleshow_description1'] = ", dalla ";
+$lang['taleequaleshow_description2'] = "pagina Facebook Tale e Quale Show ";
+$lang['taleequaleshow_description3'] = "e dalla";
+$lang['taleequaleshow_description4'] = "pagina Matteo Becucci Official Facebook";
+
+//Gadget
+$lang['gadget_title'] = "Matteo Becucci";
+$lang['gadget_subtitle'] = "Gadget";
+
+//Contatti
+$lang['contatti_title'] = "Contatti";
+$lang['contatti_social'] = "Social";
+$lang['contatti_socialfriends'] = "Social friends";
+
+
+?>
---
+++ languages/meta_it.php
@@ -0,0 +1,60 @@
+<?php
+/* 
+------------------
+Language: Italiano
+------------------
+*/
+
+$metalang = array();
+
+//Home
+$metalang['home_title'] = "Matteo Becucci Official";
+$metalang['home_description'] = "";
+$metalang['home_keywords'] = "";
+
+//Biografia
+$metalang['biografia_title'] = "Matteo Becucci Official - Biografia";
+$metalang['biografia_description'] = "";
+$metalang['biografia_keywords'] = "";
+
+//Discografia
+$metalang['discografia_title'] = "Matteo Becucci Official - Discografia";
+$metalang['discografia_description'] = "";
+$metalang['discografia_keywords'] = "";
+
+//Photogallery
+$metalang['photogallery_title'] = "Matteo Becucci Official - Foto Gallery";
+$metalang['photogallery_description'] = "";
+$metalang['photogallery_keywords'] = "";
+
+//Vocalcoach
+$metalang['vocalcoach_title'] = "Matteo Becucci Official - Vocal coach";
+$metalang['vocalcoach_description'] = "";
+$metalang['vocalcoach_keywords'] = "";
+
+//News - Eventi
+$metalang['newseventi_title'] = "Matteo Becucci Official - News Eventi";
+$metalang['newseventi_description'] = "";
+$metalang['newseventi_keywords'] = "";
+
+//Contest
+$metalang['contest_title'] = "Matteo Becucci Official - Contest";
+$metalang['contest_description'] = "";
+$metalang['contest_keywords'] = "";
+
+//Tale e quale show
+$metalang['MBlab_title'] = "Matteo Becucci Official - ";
+$metalang['MBlab_description'] = ""; 
+$metalang['MBlab_keywords'] = "";
+
+//Gadget
+$metalang['gadget_title'] = "Matteo Becucci Official - Gadget";
+$metalang['gadget_description'] = "";
+$metalang['gadget_keywords'] = "";
+
+//Contatti
+$metalang['contatti_title'] = "Matteo Becucci Official - Contatti";
+$metalang['contatti_description'] = "";
+$metalang['contatti_keywords'] = "";
+
+?>
